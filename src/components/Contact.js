d35a55b4af2d384cc04eba5a3a38a4f54e5cad86
Pierre-Alban DEMARS <demarspierrealban@gmail.com>
2019-10-23 16:04:12 +0200

    React State - Contact-State done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import './Contact.css';
 
-
-
-const Contact = props => (
-  <div className='Contact'>
-    <img src={props.avatar} alt='nothing' className='avatar'/>
-    <div>
-      <h3 className='name'>{props.name}</h3>
-      <div className='status'>
-        <p className={props.isConnected ? 'status-online' : 'status-offline'}></p>
-        <p className='status-text'>{props.isConnected ? 'Online' : 'Offline'}</p>
+class Contact extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      isConnected: false,
+    }
+  }
+  render(){
+    return (  
+      <div className='Contact'>
+        <img src={this.props.avatar} alt='nothing' className='avatar'/>
+        <div>
+          <h3 className='name'>{this.props.name}</h3>
+          <div className='status' 
+          onClick={event => {
+              const newStatus = !this.state.isConnected;
+              this.setState({ isConnected: newStatus });
+          }}>
+            <p className={this.state.isConnected ? 'status-online' : 'status-offline'}></p>
+            <p className='status-text'>{this.state.isConnected ? 'Online' : 'Offline'}</p>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-);
-
+    );
+  }
+}  
 export default Contact;
